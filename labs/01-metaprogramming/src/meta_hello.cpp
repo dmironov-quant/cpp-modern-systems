2edb55107894784df5dcef392539b109364eaf96
dmironov-quant <denisquantitativeresearcher@gmail.com>
2026-02-15 09:38:56 -0600

    updates 15 feb 2026

---
+++ labs/01-metaprogramming/src/meta_hello.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main() {
+    std::cout << "metaprogramming lab: hello\n";
+}
---
+++ templates/solution_template.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Link:
+// Topic:
+// Idea:
+// Complexity:
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}
